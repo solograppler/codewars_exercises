59a1e12877dd4139a96fe8d7ab47b6db2a3bcafa
solograppler <adalbertojnunez@gmail.com>
2021-02-02 23:56:22 -0500

    made alternate fizzbuzz

---
+++ altFizzBuzz.js
@@ -0,0 +1,15 @@
+const fizzBuzz = (x) => {
+  for (let i = 1; i <= x; i++) {
+    let output = "";
+    if (i % 3 === 0) {
+      output += "Fizz";
+    }
+    if (i % 5 === 0) {
+      output += "Buzz";
+    }
+    if (output === "") {
+      output = i;
+    }
+    console.log(output);
+  }
+};
