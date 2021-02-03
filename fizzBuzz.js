03785a995e2b724baa2998443a4585c915b4a0ec
solograppler <adalbertojnunez@gmail.com>
2021-02-02 23:06:22 -0500

    added fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+const fizzBuzz = (x) => {
+  for (let i = 1; i < x; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i, "fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log(i, "fizz");
+    } else if (i % 5 === 0) {
+      console.log(i, "buzz");
+    }
+  }
+};
+
+fizzBuzz(50);
