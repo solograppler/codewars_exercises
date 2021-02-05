e99c8268a8009c8a54aabdeca5cd4449e10f2437
solograppler <adalbertojnunez@gmail.com>
2021-02-04 23:32:58 -0500

    dashatize.js modified

--- dashatize.js
+++ dashatize.js
@@ -2,16 +2,25 @@ const dashatize = (num) => {
   let arr;
   let dashArr = [];
   if (isNaN(num)) {
-    return NaN;
+    return "NaN";
   }
   arr = String(Math.abs(num)).split("");
-  for (let i = 1; i < arr.length; i++) {
-    if (isOdd(arr[i])) {
-      dash;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[0] && arr[i] % 2 !== 0 && arr[i + 1] % 2 === 0) {
+      dashArr.push(arr[i] + "-");
+    } else if (arr[i] === arr[0] && arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0) {
+      dashArr.push(arr[i]);
+    } else if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0) {
+      dashArr.push("-" + arr[i]);
+    } else if (arr[i] % 2 !== 0 && arr[i + 1] % 2 === 0) {
+      dashArr.push("-" + arr[i] + "-");
+    } else if (arr[i] % 2 !== 0 && arr[i - 1] % 2 !== 0) {
+      dashArr.push("-" + arr[i]);
+    } else if (arr[i] % 2 !== 0 && arr[i - 1] % 2 === 0) {
+      dashArr.push("-" + arr[i]);
+    } else {
+      dashArr.push(arr[i]);
     }
   }
-
-  console.log(arr);
+  return dashArr.join("");
 };
-
-dashatize(-28369);
