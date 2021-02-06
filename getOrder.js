bdc84e9d0de63aa5bf52ce92ac6aa9826781df88
solograppler <adalbertojnunez@gmail.com>
2021-02-06 17:02:16 -0500

    getOrder.js updated to work properly

--- getOrder.js
+++ getOrder.js
@@ -1,54 +1,54 @@
-// function getOrder(input) {
-//   let menu = [
-//     "burger",
-//     "fries",
-//     "chicken",
-//     "pizza",
-//     "sandwich",
-//     "onionrings",
-//     "milkshake",
-//     "coke",
-//   ];
-//   let order = [];
-//   menu.some((word) => {
-//     if (input.includes(word)) {
-//       order.push(word);
-//     }
-//   });
-//   let fixedOrder = order.map((e) => e.charAt(0).toUpperCase() + e.slice(1));
-//   return fixedOrder.join(" ");
-// }
-
-// //getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza");
-
-// function getOrderAlt(input) {
-//   let menu = [
-//     "Burger",
-//     "Fries",
-//     "Chicken",
-//     "Pizza",
-//     "Sandwich",
-//     "Onionrings",
-//     "Milkshake",
-//     "Coke",
-//   ];
-//   result = "";
-//   menu.forEach((element) => {
-//     if (input.includes(element.toLowerCase())) {
-//       for (let i = 0; i < input.length; ++i) {
-//         result += element + " ";
-//       }
-//     }
-//   });
-//   return result;
-// }
-
-// // console.log(
-// //   getOrderAlt("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
-// // );
-
 function getOrderHi(input) {
   let menu = [
+    "burger",
+    "fries",
+    "chicken",
+    "pizza",
+    "sandwich",
+    "onionrings",
+    "milkshake",
+    "coke",
+  ];
+  let order = [];
+  menu.some((word) => {
+    if (input.includes(word)) {
+      order.push(word);
+    }
+  });
+  let fixedOrder = order.map((e) => e.charAt(0).toUpperCase() + e.slice(1));
+  return fixedOrder.join(" ");
+}
+
+//getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza");
+
+function getOrderAlt(input) {
+  let menu = [
+    "Burger",
+    "Fries",
+    "Chicken",
+    "Pizza",
+    "Sandwich",
+    "Onionrings",
+    "Milkshake",
+    "Coke",
+  ];
+  result = "";
+  menu.forEach((element) => {
+    if (input.includes(element.toLowerCase())) {
+      for (let i = 0; i < input.length; ++i) {
+        result += element + " ";
+      }
+    }
+  });
+  return result;
+}
+
+// console.log(
+//   getOrderAlt("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
+// );
+
+function getOrder(input) {
+  const menu = [
     "burger",
     "fries",
     "chicken",
@@ -62,12 +62,12 @@ function getOrderHi(input) {
   for (let i = 0; i < menu.length; i++) {
     order.push(input.match(new RegExp(`${menu[i]}`, "gi")));
   }
-  //   let fixedOrder = order.map((e) => e.charAt(0).toUpperCase() + e.slice(1));
-  //   return fixedOrder.join(" ");
-  return order;
+  return []
+    .concat(...order)
+    .filter((word) => word)
+    .map((e) => e.slice(0, 1).toUpperCase() + e.slice(1).toLowerCase())
+    .join(" ");
 }
 console.log(
-  getOrderHi("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
+  getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
 );
-
-// This code is experimental and it is being used to understand how regular expressions work.
